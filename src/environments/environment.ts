e25e47cceacf9d0970d2230a5962a1ff9e1313c0
Rahu1Singh <91598280+Rahu1Singh@users.noreply.github.com>
2023-11-05 17:23:26 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'c18b776cf4msh7cf6330309c1100p1748dbjsn7cdcae286eb5'
+};
